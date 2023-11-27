8c7687d09ab6dc13f719eca47ca4ae9ba9503991
WWatts97 <awnewatts97@outlook.com>
2023-11-26 20:10:27 -0600

    Project created

---
+++ GroceryComparer/GroceryComparer2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using GroceryComparer2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GroceryComparer2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GroceryComparer/GroceryComparer2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GroceryComparer2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
